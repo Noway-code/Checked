f8cff8533fb9606398046363929312d0dbfc8e81
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-12-17 12:19:36 +0000

    Build user schema

---
+++ backend/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+
+		password: {
+			type: String,
+			required: true,
+		},
+
+		friends: {
+			type: Array,
+			default: [],
+			required: false,
+		},
+
+		posts: {
+			type: Array,
+			default: [],
+			required: false,
+		},
+	},
+	{ collection: "Users" }
+);
+
+module.exports = mongoose.model("User", userSchema);
