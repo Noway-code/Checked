1a10d8638233cac5df71e960a849d4bb150a7f81
noway <dragonfury1928@gmail.com>
2023-12-16 19:45:40 -0500

    init branch

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
 const app = express();
+const port = process.env.PORT; // Create a .env with PORT="enter port number here"
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
