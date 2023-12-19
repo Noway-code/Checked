44bf7eeac545bdbc030c6d7da5c4824780b992f9
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-12-19 23:16:51 +0100

    Parse token to get signature

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 // Controller functions
 const { register, login } = require("../controllers/userController");
-const { addFriend, getAllFriends, removeFriend} = require("../controllers/friendController");
+const { addFriend, getAllFriends, removeFriend } = require("../controllers/friendController");
+const verifyToken = require("../middleware/verifyToken");
 
 const router = express.Router();
+
 // Login
 router.post("/login", login);
 
@@ -12,7 +14,7 @@ router.post("/login", login);
 router.post("/register", register);
 
 // Add friend
-router.post("/add-friend", addFriend);
+router.post("/add-friend", verifyToken, addFriend);
 
 // Get all friends
 router.get("/friends", getAllFriends);
