daaee620f2bed0dae6b9356643a12fc95d933013
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-12-20 00:07:43 +0100

    Add getPosts route

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,12 +1,15 @@
 const express = require("express");
 
 // Controller functions
-const { makePost } = require("../controllers/postController");
+const { makePost, getPosts } = require("../controllers/postController");
 const verifyToken = require("../middleware/verifyToken");
 
 const router = express.Router();
 
 // Make a post
 router.post("/make-post", verifyToken, makePost);
+
+// Get the user's posts
+router.get("/posts", verifyToken, getPosts);
 
 module.exports = router;
