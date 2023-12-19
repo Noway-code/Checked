d9510256c07e194a027ace5829d5fc838742a237
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-12-19 13:21:47 +0100

    Add comments

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -3,15 +3,20 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
 	const { authorization } = req.headers;
 
+	// Make sure authorization token exists
 	if (!authorization) return res.status(401).json({ error: "Authorization token required" });
 
 	try {
+		// Verify token with secret key
 		const decoded = jwt.verify(authorization, process.env.SECRET_KEY);
 
+		// Attach user info to request
 		req.user = decoded;
 
+		// Move to next middleware or route handler
 		next();
 	} catch (error) {
+		// Catch any errors (most likely an invalid token)
 		console.error(error);
 		return res.status(401).json({ error: "Unauthorized request" });
 	}
