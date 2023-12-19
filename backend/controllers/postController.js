4eccf70dab8ed33c1361b418dbaf204a8ce21521
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-12-20 00:06:49 +0100

    Create getPosts endpoint

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,5 +1,6 @@
 // Models
 const Post = require("../models/post");
+const User = require("../models/user");
 
 const makePost = async (req, res) => {
 	try {
@@ -28,6 +29,41 @@ const makePost = async (req, res) => {
 	}
 };
 
+const getPosts = async (req, res) => {
+	try {
+		const { userId } = req.user;
+
+		// Make sure that author and title is given
+		if (!userId) {
+			return res.status(400).json({ error: "User ID is missing" });
+		}
+
+		// Make sure user exists
+		const user = await User.findById(userId);
+
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		// Get all posts
+		const posts = await User.find(
+			{ _id: { $in: user.posts } },
+			{ username: 1, firstName: 1, lastName: 1 }
+		);
+
+		// Return message if no posts
+		if (posts.length === 0) {
+			return res.status(200).json({ message: "You don't have any posts." });
+		}
+
+		res.status(200).json(friends);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
 module.exports = {
 	makePost,
+	getPosts,
 };
