fbbb20045308d67afc8602805f91b1101dfec68a
noway <dragonfury1928@gmail.com>
2023-12-20 03:00:42 -0500

    Functioning remove-post

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,13 +1,19 @@
 const express = require("express");
 
 // Controller functions
-const { makePost, getPosts } = require("../controllers/postController");
+const { makePost, getPosts, deletePost, completePost } = require("../controllers/postController");
 const verifyToken = require("../middleware/verifyToken");
 
 const router = express.Router();
 
 // Make a post
 router.post("/make-post", verifyToken, makePost);
+
+// Complete a post
+router.put("/update-post", verifyToken, completePost);
+
+// Delete a post
+router.delete("/delete-post", verifyToken, deletePost);
 
 // Get the user's posts
 router.get("/posts", verifyToken, getPosts);
