d8da96e27d0b67650ea2c4e3c3af9bbb9d029f56
Camilo Alvarez-Velez <42259477+Noway-code@users.noreply.github.com>
2023-12-20 03:01:45 -0500

    Merge Complete posts (#13)
    * fixed unresolved variable
    
    * complete post updates title and description
    
    * add comments

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -34,6 +34,59 @@ const makePost = async (req, res) => {
 	}
 };
 
+
+const completePost = async (req, res) => {
+	try {
+		// Feel free to switch postId from body to params
+		// or header I wasn't sure which would be best.
+		const { userId } = req.user;
+		const { postId } = req.body;
+		const { title, description } = req.body;
+
+		// Check if the postId is provided
+		if (!postId) {
+			return res.status(400).json({ error: "Post ID is required" });
+		}
+
+		if(!title||!description){
+			return res.status(400).json({ error: "Title and description are required" });
+		}
+
+		// Find the user by ID
+		const user = await User.findById(userId)
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		// Find the post by ID
+		const post = await Post.findById(postId);
+
+		// Check if the post exists
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		// Check if the post is already completed
+		if (post.isCompleted) {
+			return res.status(400).json({ error: "Post is already completed" });
+		}
+
+		// Mark the post as completed and new post params
+		post.title = title;
+		post.description = description;
+		post.isCompleted = true;
+		post.completedAt = Date.now();
+
+		// Save the updated post
+		const savedPost = await post.save();
+
+		res.status(200).json({ message: "Post marked as completed", post: savedPost });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+}
+
 const getPosts = async (req, res) => {
 	try {
 		const { userId } = req.user;
@@ -60,4 +113,5 @@ const getPosts = async (req, res) => {
 module.exports = {
 	makePost,
 	getPosts,
+	completePost,
 };
