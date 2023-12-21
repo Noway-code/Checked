a3eef5ad652fa61c79e41f59305ab638114d8ef0
Kevin <vukevin0212@gmail.com>
2023-12-20 22:33:20 -0500

    added fields to register page

--- components/form_types/RegisterForm.js
+++ components/form_types/RegisterForm.js
@@ -6,6 +6,8 @@ export default function RegisterForm() {
    return (
       <View style={styles.formContainer}>
          <Text>This is the Register Form</Text>
+         <TextInput placeholder="First Name" style={styles.formInput}/>
+         <TextInput placeholder="Last Name" style={styles.formInput}/>
          <TextInput placeholder="Username" style={styles.formInput}/>
          <TextInput placeholder="Password" style={styles.formInput}/>
          <Button label="Register"/>
