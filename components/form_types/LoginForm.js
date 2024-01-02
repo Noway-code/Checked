576bc148439476c7909c87625c6ddbafb8bd95fa
Camilo Alvarez-Velez <42259477+Noway-code@users.noreply.github.com>
2024-01-02 00:03:52 -0500

    outside servers can connect to database (#16)

--- components/form_types/LoginForm.js
+++ components/form_types/LoginForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StyleSheet, View, Text, TextInput, ToastAndroid } from 'react-native';
+import {API_URL} from '@env';
 import Button from '../Button';
-
 import { useState } from 'react';
 import { router } from 'expo-router';
 import axios from 'axios';
@@ -10,13 +10,13 @@ export default function LoginForm() {
    const [username, setUsername] = useState("");
    const [password, setPassword] = useState("");
 
-   return (  
+   return (
       <View style={styles.formContainer}>
          <Text>This is the Login Form</Text>
-         <TextInput placeholder="Username" 
+         <TextInput placeholder="Username"
             style={styles.formInput}
             onChangeText={setUsername}/>
-         <TextInput placeholder="Password" 
+         <TextInput placeholder="Password"
             style={styles.formInput}
             onChangeText={setPassword}/>
          <Button label="Login" onPress={() => handleLogin(username, password)}/>
@@ -24,11 +24,13 @@ export default function LoginForm() {
    )
 }
 
-const handleLogin = async (user, pass) => {
+// REQUIRED: must have "API_URL=http://(your ipv4):4000" in .env file in root directory.
+// function to handle login
+const handleLogin = async (username, password) => {
    // set url for request
-   const url = "http://localhost:4000/api/user/login"
+   const url = `${API_URL}/api/user/login`;
    // set data for request
-   let obj = { "username":user, "password":pass };
+   let obj = { "username":username, "password":password };
    // turn request into json objet
    let js = JSON.stringify(obj);
 
@@ -40,28 +42,25 @@ const handleLogin = async (user, pass) => {
          "Content-Type": "application/json",
       },
       data: js,
-   }
+   };
    // call axios with data to url
    axios(config)
       .then((response) => {
          let res = response.data;
-         if (res.error)
-         {
+         if (res.error) {
             console.log(res.error);
             ToastAndroid.show(res.error, ToastAndroid.SHORT);
          }
-        else
-        {
-            console.log("Login Successful!");
+        else {
+            console.log("Login Successful! User's Token: " + res.token);
             ToastAndroid.show("Login Successful!", ToastAndroid.SHORT);
         }
-         router.replace('/')
       })
       .catch((error) => {
-         console.log(error);
+          console.log(error)
+          ToastAndroid.show("Failed", ToastAndroid.SHORT);
       });
-   
-} 
+}
 const styles = StyleSheet.create({
    formContainer: {
       padding:20,
